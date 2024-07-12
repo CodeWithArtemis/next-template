06a01b43f5b14d67f09582b5b0c133d104e109c7
CodeWithArtemis <codehunterartemis@gmail.com>
2024-07-12 12:42:56 +0530

    Eslint Husky lint-staged

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,22 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          100: "#f7fafc",
+          200: "#edf2f7",
+          300: "#e2e8f0",
+          400: "#cbd5e0",
+          500: "#a0aec0",
+          600: "#718096",
+          700: "#4a5568",
+          800: "#2d3748",
+          900: "#1a202c",
+        },
+      },
+      lineHeight: {
+        hero: "4.5rem",
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
