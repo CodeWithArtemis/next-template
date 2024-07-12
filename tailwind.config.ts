c2b14706840e899d25ef8f2fcc678714d9e406da
CodeWithArtemis <codehunterartemis@gmail.com>
2024-07-12 12:57:15 +0530

    Testing linting+ Adding prettierrc

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         gray: {
-          100: "#f7fafc",
-          200: "#edf2f7",
-          300: "#e2e8f0",
-          400: "#cbd5e0",
-          500: "#a0aec0",
-          600: "#718096",
-          700: "#4a5568",
-          800: "#2d3748",
-          900: "#1a202c",
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
         },
       },
       lineHeight: {
-        hero: "4.5rem",
+        hero: '4.5rem',
       },
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
